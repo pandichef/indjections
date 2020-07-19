e09e2d4c77510afdef54a0f88a3880da311a0220
pandichef <rashtyda@gmail.com>
2020-07-19 16:56:53 -0400

    add authentication and permissions

--- indjections/packages/djangorestframework.py
+++ indjections/packages/djangorestframework.py
@@ -1,5 +1,13 @@
 settings = """
 INSTALLED_APPS += ['rest_framework']
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [  # Ref: https://www.django-rest-framework.org/api-guide/authentication/
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 """
 
 urls = """
