6723081f2ac0963e8ae119c1c4e16cf1e9ccbddf
pandichef <rashtyda@gmail.com>
2020-07-22 04:43:28 -0400

    add djangorestframework to tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 # import pytest
 import os
+from os.path import dirname, join
 import sys
 import importlib
 # from unittest import mock
@@ -7,6 +8,10 @@ import importlib
 settings = sys.modules[os.environ['DJANGO_SETTINGS_MODULE']]
 importlib.import_module(settings.ROOT_URLCONF)
 urls = sys.modules[settings.ROOT_URLCONF]
+
+test_app_dir = join(dirname(settings.__file__), 'main')
+
+# assert False, test_app_dir
 
 # from django.contrib.auth import get_user_model
 # User = get_user_model()
@@ -20,11 +25,15 @@ def setup_and_cleanup(request):
         original_settings_string = fn.read()
     with open(urls.__file__, 'r') as fn:
         original_urls_string = fn.read()
+    with open(join(test_app_dir, 'serializers.py'), 'r') as fn:
+        original_serializers_string = fn.read()
     def restore_files():
         with open(settings.__file__, 'w') as ifn:
             ifn.write(original_settings_string)
         with open(urls.__file__, 'w') as ifn:
             ifn.write(original_urls_string)
+        with open(join(test_app_dir, 'serializers.py'), 'w') as ifn:
+            ifn.write(original_serializers_string)
     request.addfinalizer(restore_files)
     return original_settings_string, original_urls_string
 
@@ -63,8 +72,8 @@ if settings.DEBUG:
     ] + urlpatterns
 ### endblock: django-debug-toolbar ###
 """
-    assert final_settings_string == original_settings_string + settings_insertstring
-    assert final_urls_string == original_urls_string + urls_insertstring
+    assert final_settings_string[-len(settings_insertstring):] == settings_insertstring
+    assert final_urls_string[-len(urls_insertstring):] == urls_insertstring
 
 
 def test_locked(setup_and_cleanup):
@@ -72,6 +81,9 @@ def test_locked(setup_and_cleanup):
     locked_text = """### block: django-debug-toolbar/lock ###
 print('This is locked.)
 ### endblock: django-debug-toolbar ###
+### block: djangorestframework/lock ###
+print('This is locked.)
+### endblock: djangorestframework ###
 """
     with open(settings.__file__, 'w') as fn:
         fn.write(original_settings_string + locked_text)
@@ -84,5 +96,5 @@ print('This is locked.)
         final_settings_string = fn.read()
     with open(urls.__file__, 'r') as fn:
         final_urls_string = fn.read()
-    assert final_settings_string == original_settings_string + locked_text
-    assert final_urls_string == original_urls_string + locked_text
+    assert final_settings_string[-len(locked_text):] == locked_text
+    assert final_urls_string[-len(locked_text):] == locked_text
