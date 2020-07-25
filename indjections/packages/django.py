bb9213e2ff7bb2c8ea921d5b5d254f6117be8f2d
pandichef <rashtyda@gmail.com>
2020-07-25 16:57:20 -0400

    import admin/templates if base.html setting not set

--- indjections/packages/django.py
+++ indjections/packages/django.py
@@ -1,8 +1,3 @@
-import os
-import shutil
-from inspect import getfile
-import django
-
 settings = """
 TEMPLATES[0]['DIRS'] += [os.path.join(BASE_DIR, 'templates')]
 import logging
@@ -14,19 +9,3 @@ else:
     logger.setLevel(logging.INFO)
 # In each py file, include "logger = logging.getLogger(__name__)"
 """
-
-def copy_django_admin_base():
-    django_root = os.path.dirname(getfile(django))
-    project_root = os.getcwd()
-    django_admin_base = os.path.join(
-        django_root, 'contrib', 'admin', 'templates', 'admin', 'base.html')
-    project_admin_base = os.path.join(
-        project_root, 'templates', 'admin', 'base.html')
-    if not os.path.exists(project_admin_base):  # do not overwrite
-        # https://stackoverflow.com/questions/2793789/create-destination-path-for-shutil-copy-files
-        os.makedirs(os.path.dirname(project_admin_base), exist_ok=True)
-        shutil.copyfile(django_admin_base, project_admin_base)
-
-hooks = [
-    copy_django_admin_base,
-]
